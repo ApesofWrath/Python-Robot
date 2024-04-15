4b6d8557f5b00b7f1095a93bbfc13337fa7d0ddd
Nathan <nkeidel00@gmail.com>
2024-04-15 00:49:09 -0700

    Added example code from limelight repo.
    Sorry, forgot to push this!

--- hardware/vision.py
+++ hardware/vision.py
@@ -1,1 +1,31 @@
 import limelight, limelightresults
+
+from extras.debugmsgs import *
+
+class LimelightCamera:
+    '''
+    # LimelightCamera
+
+    Controlls a Limelight camera
+    '''
+    def __init__(self, id: int):
+        # Search for limelights
+        discovered_limelights = limelight.discover_limelights()
+        debugMsg(f'Discovered Limelights: {discovered_limelights}')
+
+        if discovered_limelights: # 'limelight.discover_limelights()' probably returns "None" if no limelights are discovered
+            limelight_address = discovered_limelights[0] # Get the address of the first discovered limelight
+            self.limelight = limelight.Limelight(limelight_address) # Construct a new limelight object given the address
+
+            limelight.enable_websocket() # Enable websocket in a new thread for optmized performance
+            while True:
+                result = limelight.get_latest_results()
+
+                parsed_result = limelightresults.parse_results(result)
+                if parsed_result is not None:
+                    print(parsed_result.pipeline_id)
+                    print(parsed_result.parse_latency)
+
+                    # Accessing arrays
+                    for tag in parsed_result.fiducialResults:
+                        print(tag.robot_pose_target_space, tag.fiducial_id) 
